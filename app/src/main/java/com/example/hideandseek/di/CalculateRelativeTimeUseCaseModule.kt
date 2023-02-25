1503b44bba2f410ff834f44316c05584928fba70
atomic928 <64305950+atomic928@users.noreply.github.com>
2023-02-25 09:21:33 +0900

    MyLocationRepositoryとCalculateRelativeTimeUseCaseの永続化

---
+++ app/src/main/java/com/example/hideandseek/di/CalculateRelativeTimeUseCaseModule.kt
@@ -0,0 +1,17 @@
+package com.example.hideandseek.di
+
+import com.example.hideandseek.domain.CalculateRelativeTimeUseCase
+import com.example.hideandseek.domain.CalculateRelativeTimeUseCaseImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class CalculateRelativeTimeUseCaseModule {
+    @Binds
+    abstract fun bindCalculateRelativeTimeUseCase(
+        impl: CalculateRelativeTimeUseCaseImpl
+    ): CalculateRelativeTimeUseCase
+}
