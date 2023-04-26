07af0d9bd6e536ba7d7eb6b6f71a2d54f27664b0
atomic928 <64305950+atomic928@users.noreply.github.com>
2023-04-26 14:06:56 +0900

    RoomTypeSelectFragmentViewModelのunused importantを削除

--- app/src/main/java/com/example/hideandseek/ui/viewmodel/RoomTypeSelectFragmentViewModel.kt
+++ app/src/main/java/com/example/hideandseek/ui/viewmodel/RoomTypeSelectFragmentViewModel.kt
@@ -2,7 +2,6 @@ package com.example.hideandseek.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.hideandseek.data.repository.ApiRepository
 import com.example.hideandseek.data.repository.MyInfoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
