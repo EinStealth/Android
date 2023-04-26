5c4ab190d6654755f4e80a64b90d6b24743a6af6
atomic928 <64305950+atomic928@users.noreply.github.com>
2023-04-26 13:37:14 +0900

    RegisterUserIconFragmentの修正

--- app/src/main/java/com/example/hideandseek/ui/view/RegisterUserIconFragment.kt
+++ app/src/main/java/com/example/hideandseek/ui/view/RegisterUserIconFragment.kt
@@ -1,11 +1,9 @@
 package com.example.hideandseek.ui.view
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -13,11 +11,10 @@ import androidx.navigation.fragment.findNavController
 import com.example.hideandseek.R
 import com.example.hideandseek.databinding.FragmentRegisterUserIconBinding
 import com.example.hideandseek.ui.viewmodel.RegisterUserIconFragmentViewModel
-import com.example.hideandseek.ui.viewmodel.RegisterUserNameFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class RegisterUserIconFragment: Fragment() {
+class RegisterUserIconFragment : Fragment() {
     private var _binding: FragmentRegisterUserIconBinding? = null
     private val viewModel: RegisterUserIconFragmentViewModel by viewModels()
 
