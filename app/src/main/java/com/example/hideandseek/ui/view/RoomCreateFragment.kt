3ccf20db1a55f8af6353d61dd37b6be8330f770f
atomic <daimu0307misutiru@gmail.com>
2023-04-27 22:17:50 +0900

    clickしたときの処理を実装

--- app/src/main/java/com/example/hideandseek/ui/view/RoomCreateFragment.kt
+++ app/src/main/java/com/example/hideandseek/ui/view/RoomCreateFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
@@ -29,57 +27,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.fragment.findNavController
 import com.example.hideandseek.R
-import com.example.hideandseek.databinding.FragmentRoomCreateBinding
-import com.example.hideandseek.ui.viewmodel.RegisterUserNameFragmentViewModel
 import com.example.hideandseek.ui.viewmodel.RoomCreateFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RoomCreateFragment : Fragment() {
-    private var _binding: FragmentRoomCreateBinding? = null
-    private val viewModel: RoomCreateFragmentViewModel by viewModels()
-
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = FragmentRoomCreateBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        // Viewの取得
-        val btCreate: ImageView = binding.btCreate
-        val editSecretWord: EditText = binding.editSecretWord
-
-        // 名前・アイコンの読み込み
-        viewModel.readUserInfo()
-
-        btCreate.setOnClickListener {
-            // 部屋の作成
-            viewModel.postRoom(editSecretWord.text.toString())
-            viewLifecycleOwner.lifecycleScope.launch {
-                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    viewModel.uiState.collect {
-                        // player情報のpost
-                        viewModel.postPlayer(editSecretWord.text.toString(), it.userName, it.userIcon)
-                    }
-                }
-            }
-            // secret_wordsの保存
-            viewModel.writeSecretWords(editSecretWord.text.toString())
-            findNavController().navigate(R.id.navigation_stand_by_room)
-        }
-
         return ComposeView(requireContext()).apply {
             setContent {
                 RoomCreateScreen(
@@ -92,6 +52,9 @@ class RoomCreateFragment : Fragment() {
 
 @Composable
 fun RoomCreateScreen(onNavigate: (Int) -> (Unit), viewModel: RoomCreateFragmentViewModel = viewModel()) {
+    // 名前・アイコンの読み込み
+    viewModel.readUserInfo()
+
     Surface(Modifier.fillMaxSize()) {
         Image(
             painter = painterResource(R.drawable.title_background_responsive_nontitlever),
@@ -100,15 +63,19 @@ fun RoomCreateScreen(onNavigate: (Int) -> (Unit), viewModel: RoomCreateFragmentV
         )
         ConstraintLayout {
             // Create references for the composable to constrain
-            val (dialog, btDecide, textField) = createRefs()
+            val (dialog, btCreate, textField) = createRefs()
 
             Image(
-                painter = painterResource(R.drawable.text_null),
+                painter = painterResource(R.drawable.secret_word),
                 contentDescription = "dialog",
                 modifier = Modifier.constrainAs(dialog) {
                     top.linkTo(parent.top)
+                    end.linkTo(parent.end)
                     bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
                 }
+                    .width(300.dp)
+                    .height(258.dp)
             )
             var text by remember { mutableStateOf("") }
 
@@ -118,7 +85,7 @@ fun RoomCreateScreen(onNavigate: (Int) -> (Unit), viewModel: RoomCreateFragmentV
                 colors = TextFieldDefaults.textFieldColors(
                     backgroundColor = Color.Transparent
                 ),
-                placeholder = { Text(text = "名前を入力して下さい") },
+                placeholder = { Text(text = "\uD83D\uDDDD　合言葉") },
                 modifier = Modifier.constrainAs(textField) {
                     top.linkTo(dialog.top)
                     end.linkTo(dialog.end)
@@ -126,12 +93,11 @@ fun RoomCreateScreen(onNavigate: (Int) -> (Unit), viewModel: RoomCreateFragmentV
                     start.linkTo(dialog.start)
                 }
             )
-
             Image(
-                painter = painterResource(R.drawable.button_decide),
-                contentDescription = "decide_button",
+                painter = painterResource(R.drawable.bt_create),
+                contentDescription = "create_button",
                 modifier = Modifier
-                    .constrainAs(btDecide) {
+                    .constrainAs(btCreate) {
                         top.linkTo(textField.bottom)
                         end.linkTo(dialog.end)
                         bottom.linkTo(dialog.bottom)
@@ -139,6 +105,15 @@ fun RoomCreateScreen(onNavigate: (Int) -> (Unit), viewModel: RoomCreateFragmentV
                     }
                     .width(142.dp)
                     .height(72.dp)
+                    .clickable {
+                        // 部屋の作成
+                        viewModel.postRoom(text)
+                        // player情報のpost
+                        viewModel.postPlayer(text, viewModel.uiState.value.userName, viewModel.uiState.value.userIcon)
+                        // secret_wordsの保存
+                        viewModel.writeSecretWords(text)
+                        onNavigate(R.id.navigation_stand_by_room)
+                    }
             )
         }
     }
@@ -149,7 +124,7 @@ fun RoomCreateScreen(onNavigate: (Int) -> (Unit), viewModel: RoomCreateFragmentV
 fun RoomCreatePreview() {
     ConstraintLayout {
         // Create references for the composable to constrain
-        val (background, dialog, btDecide, textField) = createRefs()
+        val (background, dialog, btCreate, textField) = createRefs()
 
         Image(
             painter = painterResource(R.drawable.title_background_responsive_nontitlever),
@@ -191,7 +166,7 @@ fun RoomCreatePreview() {
             painter = painterResource(R.drawable.bt_create),
             contentDescription = "create_button",
             modifier = Modifier
-                .constrainAs(btDecide) {
+                .constrainAs(btCreate) {
                     top.linkTo(textField.bottom)
                     end.linkTo(dialog.end)
                     bottom.linkTo(dialog.bottom)
