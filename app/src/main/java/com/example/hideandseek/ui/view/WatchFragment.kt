be9bb9488cfca60cd15020e97f7e674fc1127b39
atomic <daimu0307misutiru@gmail.com>
2023-04-28 22:19:36 +0900

    WatchFragmentでMapを表示できるようにした

--- app/src/main/java/com/example/hideandseek/ui/view/WatchFragment.kt
+++ app/src/main/java/com/example/hideandseek/ui/view/WatchFragment.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,6 +31,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.example.hideandseek.R
 import com.example.hideandseek.databinding.FragmentWatchBinding
 import com.example.hideandseek.ui.viewmodel.StandByRoomFragmentViewModel
@@ -102,7 +104,13 @@ class WatchFragment(
             }
         }
 
-        return root
+        return ComposeView(requireContext()).apply {
+            setContent {
+                WatchScreen(
+                    onNavigate = { dest -> findNavController().navigate(dest) }
+                )
+            }
+        }
     }
 }
 
