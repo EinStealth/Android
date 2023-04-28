51e8beb5f199f996018e5ab61036e554ff1ab4ef
atomic928 <64305950+atomic928@users.noreply.github.com>
2023-04-28 17:30:46 +0900

    StandByFragmentのレイアウトを作成

--- app/src/main/java/com/example/hideandseek/ui/view/StandByRoomFragment.kt
+++ app/src/main/java/com/example/hideandseek/ui/view/StandByRoomFragment.kt
@@ -7,6 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Visibility
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -110,5 +128,145 @@ class StandByRoomFragment : Fragment() {
                 R.drawable.user04_normal
             }
         }
+    }
+}
+
+@Composable
+@Preview
+fun StandByRoomPreview() {
+    ConstraintLayout {
+        // Create references for the composable to constrain
+        val (background, dialog, textSecret, icon1, icon2, icon3, icon4, name1, name2, name3, name4, btStart) = createRefs()
+
+        Image(
+            painter = painterResource(R.drawable.title_background_responsive_nontitlever),
+            contentDescription = "background",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .constrainAs(background) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                }
+        )
+        Image(
+            painter = painterResource(R.drawable.stand_by_room),
+            contentDescription = "dialog",
+            modifier = Modifier
+                .constrainAs(dialog) {
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                }
+                .width(302.dp)
+                .height(405.dp)
+        )
+        Text(
+            text = "secretWords",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .constrainAs(textSecret) {
+                    top.linkTo(dialog.top)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                }
+                .padding(top = 44.dp)
+        )
+        Image(
+            painter = painterResource(R.drawable.user01_normal),
+            contentDescription = "icon1",
+            modifier = Modifier
+                .constrainAs(icon1) {
+                    top.linkTo(dialog.top)
+                    start.linkTo(dialog.start)
+                }
+                .padding(top = 80.dp, start = 12.dp)
+                .height(100.dp)
+                .width(100.dp)
+        )
+        Text(
+            text = "name1",
+            modifier = Modifier
+                .constrainAs(name1) {
+                    top.linkTo(icon1.bottom)
+                    start.linkTo(icon1.start)
+                    end.linkTo(icon1.end)
+                }
+        )
+        Image(
+            painter = painterResource(R.drawable.user01_normal),
+            contentDescription = "icon2",
+            modifier = Modifier
+                .constrainAs(icon2) {
+                    top.linkTo(dialog.top)
+                    start.linkTo(icon1.end)
+                }
+                .padding(top = 80.dp, start = 12.dp)
+                .height(100.dp)
+                .width(100.dp)
+        )
+        Text(
+            text = "name2",
+            modifier = Modifier
+                .constrainAs(name2) {
+                    top.linkTo(icon1.bottom)
+                    start.linkTo(icon2.start)
+                    end.linkTo(icon2.end)
+                }
+        )
+        Image(
+            painter = painterResource(R.drawable.user01_normal),
+            contentDescription = "icon3",
+            modifier = Modifier
+                .constrainAs(icon3) {
+                    top.linkTo(name1.bottom)
+                    start.linkTo(dialog.start)
+                }
+                .height(100.dp)
+                .width(100.dp)
+        )
+        Text(
+            text = "name3",
+            modifier = Modifier
+                .constrainAs(name3) {
+                    top.linkTo(icon3.bottom)
+                    start.linkTo(icon3.start)
+                    end.linkTo(icon3.end)
+                }
+        )
+        Image(
+            painter = painterResource(R.drawable.user01_normal),
+            contentDescription = "icon4",
+            modifier = Modifier
+                .constrainAs(icon4) {
+                    top.linkTo(name2.bottom)
+                    start.linkTo(icon3.end)
+                }
+                .padding(start = 12.dp)
+                .height(100.dp)
+                .width(100.dp)
+        )
+        Text(
+            text = "name4",
+            modifier = Modifier
+                .constrainAs(name4) {
+                    top.linkTo(icon4.bottom)
+                    start.linkTo(icon4.start)
+                    end.linkTo(icon4.end)
+                }
+        )
+        Image(
+            painter = painterResource(R.drawable.bt_start),
+            contentDescription = "button_start",
+            modifier = Modifier
+                .constrainAs(btStart) {
+                    end.linkTo(parent.end)
+                    bottom.linkTo(dialog.bottom)
+                    start.linkTo(parent.start)
+                }
+                .padding(bottom = 12.dp)
+                .width(142.dp)
+                .height(72.dp)
+        )
     }
 }
