b237504ca8eb90ee7bfda055e1a55055fd163691
atomic <daimu0307misutiru@gmail.com>
2023-04-28 22:14:56 +0900

    WatchFragmentのUI作成

--- app/src/main/java/com/example/hideandseek/ui/view/WatchFragment.kt
+++ app/src/main/java/com/example/hideandseek/ui/view/WatchFragment.kt
@@ -6,12 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.example.hideandseek.R
 import com.example.hideandseek.databinding.FragmentWatchBinding
+import com.example.hideandseek.ui.viewmodel.StandByRoomFragmentViewModel
 import com.example.hideandseek.ui.viewmodel.WatchFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineDispatcher
@@ -82,5 +103,204 @@ class WatchFragment(
         }
 
         return root
+    }
+}
+
+@Composable
+fun WatchScreen(onNavigate: (Int) -> (Unit), viewModel: WatchFragmentViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
+    val watchUiState by viewModel.uiState.collectAsState()
+
+    Surface(Modifier.fillMaxSize()) {
+        watchUiState.map?.let {
+            Image(
+                bitmap = it.asImageBitmap(),
+                contentDescription = "map",
+                contentScale = ContentScale.Crop
+            )
+        }
+        ConstraintLayout {
+            // Create references for the composable to constrain
+            val (ivWatching, btCaptureOff, btSkillOf, user1, user2, user3, user4) = createRefs()
+
+            Image(
+                painter = painterResource(R.drawable.text_watching),
+                contentDescription = "text_watching",
+                modifier = Modifier
+                    .constrainAs(ivWatching) {
+                        end.linkTo(parent.end)
+                        bottom.linkTo(btCaptureOff.top)
+                        start.linkTo(parent.start)
+                    }
+                    .height(80.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.button_captured_off),
+                contentDescription = "鬼に捕まったときに押すボタン",
+                modifier = Modifier
+                    .constrainAs(btCaptureOff) {
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .height(100.dp)
+                    .width(200.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.button_skill_off),
+                contentDescription = "skill button",
+                modifier = Modifier
+                    .constrainAs(btSkillOf) {
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                    }
+                    .height(100.dp)
+                    .width(180.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.user01_caputure),
+                contentDescription = "user1",
+                modifier = Modifier
+                    .constrainAs(user1) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
+                    .padding(start = 40.dp)
+                    .height(72.dp)
+                    .width(72.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.user02_runaway),
+                contentDescription = "user2",
+                modifier = Modifier
+                    .constrainAs(user2) {
+                        top.linkTo(user1.top)
+                        end.linkTo(user3.start)
+                        start.linkTo(user1.end)
+                    }
+                    .height(72.dp)
+                    .width(72.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.user03_runaway),
+                contentDescription = "user3",
+                modifier = Modifier
+                    .constrainAs(user3) {
+                        top.linkTo(user1.top)
+                        end.linkTo(user4.start)
+                        start.linkTo(user2.end)
+                    }
+                    .height(72.dp)
+                    .width(72.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.user04_oni),
+                contentDescription = "user4",
+                modifier = Modifier
+                    .constrainAs(user4) {
+                        top.linkTo(user1.top)
+                        end.linkTo(parent.end)
+                    }
+                    .padding(end = 40.dp)
+                    .height(72.dp)
+                    .width(72.dp)
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun WatchPreview() {
+    Surface(Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.title_background_responsive_nontitlever),
+            contentDescription = "map",
+            contentScale = ContentScale.Crop
+        )
+        ConstraintLayout {
+            // Create references for the composable to constrain
+            val (ivWatching, btCaptureOff, btSkillOf, user1, user2, user3, user4) = createRefs()
+
+            Image(
+                painter = painterResource(R.drawable.text_watching),
+                contentDescription = "text_watching",
+                modifier = Modifier
+                    .constrainAs(ivWatching) {
+                        end.linkTo(parent.end)
+                        bottom.linkTo(btCaptureOff.top)
+                        start.linkTo(parent.start)
+                    }
+                    .height(80.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.button_captured_off),
+                contentDescription = "鬼に捕まったときに押すボタン",
+                modifier = Modifier
+                    .constrainAs(btCaptureOff) {
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .height(100.dp)
+                    .width(200.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.button_skill_off),
+                contentDescription = "skill button",
+                modifier = Modifier
+                    .constrainAs(btSkillOf) {
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                    }
+                    .height(100.dp)
+                    .width(180.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.user01_caputure),
+                contentDescription = "user1",
+                modifier = Modifier
+                    .constrainAs(user1) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
+                    .padding(start = 40.dp)
+                    .height(72.dp)
+                    .width(72.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.user02_runaway),
+                contentDescription = "user2",
+                modifier = Modifier
+                    .constrainAs(user2) {
+                        top.linkTo(user1.top)
+                        end.linkTo(user3.start)
+                        start.linkTo(user1.end)
+                    }
+                    .height(72.dp)
+                    .width(72.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.user03_runaway),
+                contentDescription = "user3",
+                modifier = Modifier
+                    .constrainAs(user3) {
+                        top.linkTo(user1.top)
+                        end.linkTo(user4.start)
+                        start.linkTo(user2.end)
+                    }
+                    .height(72.dp)
+                    .width(72.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.user04_oni),
+                contentDescription = "user4",
+                modifier = Modifier
+                    .constrainAs(user4) {
+                        top.linkTo(user1.top)
+                        end.linkTo(parent.end)
+                    }
+                    .padding(end = 40.dp)
+                    .height(72.dp)
+                    .width(72.dp)
+            )
+        }
     }
 }
