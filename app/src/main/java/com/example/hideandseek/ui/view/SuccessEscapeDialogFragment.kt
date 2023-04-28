bcbb4a07645cb70c936693b9f9d76d423af595d7
atomic928 <64305950+atomic928@users.noreply.github.com>
2023-04-28 15:39:57 +0900

    SuccessEscapeDialogFragmentのレイアウトを作成

--- app/src/main/java/com/example/hideandseek/ui/view/SuccessEscapeDialogFragment.kt
+++ app/src/main/java/com/example/hideandseek/ui/view/SuccessEscapeDialogFragment.kt
@@ -8,10 +8,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.fragment.findNavController
 import com.example.hideandseek.R
 import com.example.hideandseek.databinding.FragmentSuccessEscapeDialogBinding
+import com.example.hideandseek.ui.viewmodel.CaptureDialogViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -46,5 +59,104 @@ class SuccessEscapeDialogFragment : DialogFragment() {
         dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
         dialog.window?.setDimAmount(0f)
         return dialog
+    }
+}
+
+@Composable
+fun SuccessEscapeDialogScreen(onNavigate: (Int) -> (Unit), viewModel: CaptureDialogViewModel = viewModel(), d: Dialog?) {
+    ConstraintLayout {
+        // Create references for the composable to constrain
+        val (dialog, icon, btClose) = createRefs()
+
+        Image(
+            painter = painterResource(R.drawable.text_clear),
+            contentDescription = "dialog",
+            modifier = Modifier
+                .constrainAs(dialog) {
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                }
+                .width(400.dp)
+                .height(352.dp)
+        )
+        Image(
+            painter = painterResource(R.drawable.user01_clear),
+            contentDescription = "demon",
+            modifier = Modifier
+                .constrainAs(icon) {
+                    top.linkTo(dialog.top)
+                    end.linkTo(dialog.end)
+                    bottom.linkTo(dialog.bottom)
+                    start.linkTo(dialog.start)
+                }
+                .padding(end = 20.dp)
+                .width(160.dp)
+                .height(160.dp)
+        )
+        Image(
+            painter = painterResource(R.drawable.button_close),
+            contentDescription = "button_close",
+            modifier = Modifier
+                .constrainAs(btClose) {
+                    start.linkTo(dialog.start)
+                    end.linkTo(dialog.end)
+                    bottom.linkTo(dialog.bottom)
+                }
+                .padding(bottom = 20.dp)
+                .width(160.dp)
+                .height(80.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SuccessEscapeDialogPreview() {
+    ConstraintLayout {
+        // Create references for the composable to constrain
+        val (dialog, icon, btClose) = createRefs()
+
+        Image(
+            painter = painterResource(R.drawable.text_clear),
+            contentDescription = "dialog",
+            modifier = Modifier
+                .constrainAs(dialog) {
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                }
+                .width(400.dp)
+                .height(352.dp)
+        )
+        Image(
+            painter = painterResource(R.drawable.user01_clear),
+            contentDescription = "demon",
+            modifier = Modifier
+                .constrainAs(icon) {
+                    top.linkTo(dialog.top)
+                    end.linkTo(dialog.end)
+                    bottom.linkTo(dialog.bottom)
+                    start.linkTo(dialog.start)
+                }
+                .padding(end = 20.dp)
+                .width(160.dp)
+                .height(160.dp)
+        )
+        Image(
+            painter = painterResource(R.drawable.button_close),
+            contentDescription = "button_close",
+            modifier = Modifier
+                .constrainAs(btClose) {
+                    start.linkTo(dialog.start)
+                    end.linkTo(dialog.end)
+                    bottom.linkTo(dialog.bottom)
+                }
+                .padding(bottom = 20.dp)
+                .width(160.dp)
+                .height(80.dp)
+        )
     }
 }
