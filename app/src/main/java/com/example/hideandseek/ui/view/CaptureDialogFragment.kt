396ca406a2120567641c0051d0f073a3ec525a5f
atomic <daimu0307misutiru@gmail.com>
2023-04-30 12:12:24 +0900

    クリック処理を実装

--- app/src/main/java/com/example/hideandseek/ui/view/CaptureDialogFragment.kt
+++ app/src/main/java/com/example/hideandseek/ui/view/CaptureDialogFragment.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -72,7 +71,7 @@ fun CaptureDialogScreen(viewModel: CaptureDialogViewModel = viewModel(), navCont
                     .clickable {
                         // ステータスを捕まったに変更
                         viewModel.updatePlayerStatus(3)
-//                    navController.navigate("notifyCapture")
+                        navController.navigate("notifyCapture")
                     }
             )
             Image(
