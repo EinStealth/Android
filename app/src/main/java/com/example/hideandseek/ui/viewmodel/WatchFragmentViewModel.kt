e659efa03da9a1c877ad971a84e3f872f8130de3
atomic <daimu0307misutiru@gmail.com>
2023-05-14 14:10:34 +0900

    ゲーム画面でのplayerもlazyRowに変更

--- app/src/main/java/com/example/hideandseek/ui/viewmodel/WatchFragmentViewModel.kt
+++ app/src/main/java/com/example/hideandseek/ui/viewmodel/WatchFragmentViewModel.kt
@@ -7,6 +7,8 @@ import androidx.lifecycle.viewModelScope
 import com.example.hideandseek.data.datasource.local.LocationData
 import com.example.hideandseek.data.datasource.local.TrapData
 import com.example.hideandseek.data.datasource.local.UserData
+import com.example.hideandseek.data.datasource.remote.ResponseData
+import com.example.hideandseek.data.repository.ApiRepository
 import com.example.hideandseek.data.repository.LocationRepository
 import com.example.hideandseek.data.repository.MapRepository
 import com.example.hideandseek.data.repository.MyInfoRepository
@@ -24,6 +26,7 @@ data class WatchUiState(
     val allLocation: List<LocationData> = listOf(),
     val latestUser: UserData = UserData(0, "", 0.0, 0.0, 0.0),
     val allTrap: List<TrapData> = listOf(),
+    val allPlayer: List<ResponseData.ResponseGetPlayer> = listOf(),
     val map: Bitmap? = null,
     val preRelativeTime: String = ""
 )
@@ -33,7 +36,8 @@ class WatchFragmentViewModel @Inject constructor(
     locationRepository: LocationRepository,
     private val trapRepository: TrapRepository,
     private val mapRepository: MapRepository,
-    private val myInfoRepository: MyInfoRepository
+    private val myInfoRepository: MyInfoRepository,
+    private val apiRepository: ApiRepository
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(WatchUiState())
     val uiState: StateFlow<WatchUiState> = _uiState.asStateFlow()
@@ -64,6 +68,35 @@ class WatchFragmentViewModel @Inject constructor(
                 _uiState.update { mainUiState ->
                     mainUiState.copy(allTrap = allTraps)
                 }
+            }
+        }
+        viewModelScope.launch {
+            while (true) {
+                val secretWords = myInfoRepository.readSecretWords()
+                getPlayer(secretWords)
+                delay(100)
+            }
+        }
+    }
+
+    private fun getPlayer(secretWords: String) {
+        viewModelScope.launch {
+            try {
+                val response = apiRepository.getPlayer(secretWords)
+                if (response.isSuccessful) {
+                    if (response.body() != null) {
+                        viewModelScope.launch {
+                            _uiState.update { mainUiState ->
+                                mainUiState.copy(allPlayer = response.body()!!)
+                            }
+                        }
+                    }
+                    Log.d("GET_TEST_PLAYER", "${response}\n${response.body()}")
+                } else {
+                    Log.d("GET_TEST_PLAYER", "$response")
+                }
+            } catch (e: java.lang.Exception) {
+                Log.d("GET_TEST_PLAYER", "$e")
             }
         }
     }
