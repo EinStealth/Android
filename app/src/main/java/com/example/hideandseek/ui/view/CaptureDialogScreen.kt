3e7b5739a5abec9a9f8f8dd64432cac01d096127
atomic <daimu0307misutiru@gmail.com>
2023-05-24 14:41:33 +0900

    CaptureDialogのPreviewに実際の関数を使用

--- app/src/main/java/com/example/hideandseek/ui/view/CaptureDialogScreen.kt
+++ app/src/main/java/com/example/hideandseek/ui/view/CaptureDialogScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -16,11 +17,23 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.hideandseek.R
 import com.example.hideandseek.ui.viewmodel.CaptureDialogViewModel
 
 @Composable
 fun CaptureDialogScreen(viewModel: CaptureDialogViewModel = viewModel(), navController: NavController) {
+    CaptureDialogLayout(
+        navController = navController,
+        updateStatus = { viewModel.updatePlayerStatus(1) }
+    )
+}
+
+@Composable
+fun CaptureDialogLayout(
+    navController: NavController,
+    updateStatus: () -> Unit,
+) {
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -70,7 +83,7 @@ fun CaptureDialogScreen(viewModel: CaptureDialogViewModel = viewModel(), navCont
                     .height(80.dp)
                     .clickable {
                         // ステータスを捕まったに変更
-                        viewModel.updatePlayerStatus(1)
+                        updateStatus()
                         navController.navigate("notifyCapture")
                     }
             )
@@ -100,60 +113,9 @@ fun CaptureDialogScreen(viewModel: CaptureDialogViewModel = viewModel(), navCont
 @Preview
 @Composable
 fun CaptureDialogPreview() {
-    ConstraintLayout {
-        // Create references for the composable to constrain
-        val (dialog, icon, btYes, btNo) = createRefs()
-
-        Image(
-            painter = painterResource(R.drawable.text_captureboolean),
-            contentDescription = "dialog",
-            modifier = Modifier
-                .constrainAs(dialog) {
-                    top.linkTo(parent.top)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
-                    start.linkTo(parent.start)
-                }
-                .width(400.dp)
-                .height(352.dp)
-        )
-        Image(
-            painter = painterResource(R.drawable.user04_oni),
-            contentDescription = "icon",
-            modifier = Modifier
-                .constrainAs(icon) {
-                    top.linkTo(dialog.top)
-                    end.linkTo(dialog.end)
-                    bottom.linkTo(dialog.bottom)
-                    start.linkTo(dialog.start)
-                }
-                .width(160.dp)
-                .height(160.dp)
-                .padding(end = 20.dp)
-        )
-        Image(
-            painter = painterResource(R.drawable.button_yes),
-            contentDescription = "button_yes",
-            modifier = Modifier
-                .constrainAs(btYes) {
-                    end.linkTo(dialog.end)
-                    bottom.linkTo(dialog.bottom)
-                }
-                .width(160.dp)
-                .height(80.dp)
-                .padding(end = 20.dp, bottom = 20.dp)
-        )
-        Image(
-            painter = painterResource(R.drawable.button_no),
-            contentDescription = "button_no",
-            modifier = Modifier
-                .constrainAs(btNo) {
-                    bottom.linkTo(dialog.bottom)
-                    start.linkTo(dialog.start)
-                }
-                .width(160.dp)
-                .height(80.dp)
-                .padding(start = 20.dp, bottom = 20.dp)
-        )
-    }
+    val navController = rememberNavController()
+    CaptureDialogLayout(
+        navController = navController,
+        updateStatus = {}
+    )
 }
