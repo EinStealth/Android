7919643f42ca3c1810e539168269dd3cc39dec9f
atomic <daimu0307misutiru@gmail.com>
2023-05-24 15:15:42 +0900

    RegisterUserIconのPreviewに実際の関数を使用

--- app/src/main/java/com/example/hideandseek/ui/view/RegisterUserIconScreen.kt
+++ app/src/main/java/com/example/hideandseek/ui/view/RegisterUserIconScreen.kt
@@ -18,11 +18,21 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.hideandseek.R
 import com.example.hideandseek.ui.viewmodel.RegisterUserIconFragmentViewModel
 
 @Composable
 fun RegisterUserIconScreen(viewModel: RegisterUserIconFragmentViewModel = viewModel(), navController: NavController) {
+    RegisterUserIconLayout(navController = navController, writeUserIcon = { viewModel.writeUserIcon(it) })
+}
+
+@Composable
+private fun RegisterUserIconLayout(
+    navController: NavController,
+    writeUserIcon: (Int) -> Unit,
+
+) {
     Surface(Modifier.fillMaxSize()) {
         Image(
             painter = painterResource(R.drawable.title_background_responsive_nontitlever),
@@ -41,54 +51,59 @@ fun RegisterUserIconScreen(viewModel: RegisterUserIconFragmentViewModel = viewMo
                 }
             )
             Row(
-                modifier = Modifier.constrainAs(iconList) {
-                    top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
-                }
+                modifier = Modifier
+                    .constrainAs(iconList) {
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    }
                     .fillMaxWidth(1f),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 Image(
                     painter = painterResource(R.drawable.user01_normal),
                     contentDescription = "icon1",
-                    modifier = Modifier.width(92.dp)
+                    modifier = Modifier
+                        .width(92.dp)
                         .height(92.dp)
                         .clickable {
                             // アイコンの保存
-                            viewModel.writeUserIcon(1)
+                            writeUserIcon(1)
                             navController.navigate("roomTypeSelect")
                         }
                 )
                 Image(
                     painter = painterResource(R.drawable.user02_normal),
                     contentDescription = "icon2",
-                    modifier = Modifier.width(92.dp)
+                    modifier = Modifier
+                        .width(92.dp)
                         .height(92.dp)
                         .clickable {
                             // アイコンの保存
-                            viewModel.writeUserIcon(2)
+                            writeUserIcon(2)
                             navController.navigate("roomTypeSelect")
                         }
                 )
                 Image(
                     painter = painterResource(R.drawable.user03_normal),
                     contentDescription = "icon3",
-                    modifier = Modifier.width(92.dp)
+                    modifier = Modifier
+                        .width(92.dp)
                         .height(92.dp)
                         .clickable {
                             // アイコンの保存
-                            viewModel.writeUserIcon(3)
+                            writeUserIcon(3)
                             navController.navigate("roomTypeSelect")
                         }
                 )
                 Image(
                     painter = painterResource(R.drawable.user04_normal),
                     contentDescription = "icon4",
-                    modifier = Modifier.width(92.dp)
+                    modifier = Modifier
+                        .width(92.dp)
                         .height(92.dp)
                         .clickable {
                             // アイコンの保存
-                            viewModel.writeUserIcon(4)
+                            writeUserIcon(4)
                             navController.navigate("roomTypeSelect")
                         }
                 )
@@ -99,59 +114,6 @@ fun RegisterUserIconScreen(viewModel: RegisterUserIconFragmentViewModel = viewMo
 
 @Preview
 @Composable
-fun RegisterUserIconPreview() {
-    ConstraintLayout {
-        // Create references for the composable to constrain
-        val (background, dialog, iconList) = createRefs()
-
-        Image(
-            painter = painterResource(R.drawable.title_background_responsive_nontitlever),
-            contentDescription = "background",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.constrainAs(background) {
-                top.linkTo(parent.top)
-            }
-        )
-        Image(
-            painter = painterResource(R.drawable.text_null),
-            contentDescription = "dialog",
-            modifier = Modifier.constrainAs(dialog) {
-                top.linkTo(parent.top)
-                bottom.linkTo(parent.bottom)
-            }
-        )
-        Row(
-            modifier = Modifier.constrainAs(iconList) {
-                top.linkTo(parent.top)
-                bottom.linkTo(parent.bottom)
-            }
-                .fillMaxWidth(1f),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Image(
-                painter = painterResource(R.drawable.user01_normal),
-                contentDescription = "icon1",
-                modifier = Modifier.width(92.dp)
-                    .height(92.dp)
-            )
-            Image(
-                painter = painterResource(R.drawable.user02_normal),
-                contentDescription = "icon2",
-                modifier = Modifier.width(92.dp)
-                    .height(92.dp)
-            )
-            Image(
-                painter = painterResource(R.drawable.user03_normal),
-                contentDescription = "icon3",
-                modifier = Modifier.width(92.dp)
-                    .height(92.dp)
-            )
-            Image(
-                painter = painterResource(R.drawable.user04_normal),
-                contentDescription = "icon4",
-                modifier = Modifier.width(92.dp)
-                    .height(92.dp)
-            )
-        }
-    }
+private fun RegisterUserIconPreview() {
+    RegisterUserIconLayout(navController = rememberNavController(), writeUserIcon = {})
 }
