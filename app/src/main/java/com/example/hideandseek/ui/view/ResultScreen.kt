51cba237435f48dcab7aefee1e0998b6ce837a89
atomic <daimu0307misutiru@gmail.com>
2023-05-26 15:19:53 +0900

    コード削減のためResultScreenのConstraintを削除

--- app/src/main/java/com/example/hideandseek/ui/view/ResultScreen.kt
+++ app/src/main/java/com/example/hideandseek/ui/view/ResultScreen.kt
@@ -1,12 +1,18 @@
 package com.example.hideandseek.ui.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
@@ -23,119 +29,86 @@ fun ResultScreen() {
 
 @Composable
 private fun ResultLayout() {
-    ConstraintLayout {
-        // Create references for the composable to constrain
-        val (dialog, textResult, textWinner, textLoser, winner1, winner2, loser1, loser2, btClose) = createRefs()
-
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
         Image(
             painter = painterResource(R.drawable.text_null),
             contentDescription = "dialog",
             modifier = Modifier
-                .constrainAs(dialog) {
-                    top.linkTo(parent.top)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
-                    start.linkTo(parent.start)
-                }
                 .scale(scaleX = 1f, scaleY = 2f)
                 .fillMaxHeight()
                 .padding(10.dp)
         )
-        Text(
-            text = "RESULT",
-            fontSize = 32.sp,
-            modifier = Modifier
-                .constrainAs(textResult) {
-                    end.linkTo(parent.end)
-                    bottom.linkTo(textWinner.top)
-                    start.linkTo(parent.start)
-                }
-                .padding(bottom = 12.dp)
-        )
-        Text(
-            text = "WINNER",
-            fontSize = 32.sp,
-            modifier = Modifier
-                .constrainAs(textWinner) {
-                    top.linkTo(dialog.top)
-                    bottom.linkTo(textLoser.top)
-                    start.linkTo(dialog.start)
-                }
-                .padding(start = 32.dp)
-        )
-        Image(
-            painter = painterResource(R.drawable.user01_normal),
-            contentDescription = "winner1",
-            modifier = Modifier
-                .constrainAs(winner1) {
-                    top.linkTo(textWinner.bottom)
-                    bottom.linkTo(textLoser.top)
-                    start.linkTo(textWinner.start)
-                }
-                .height(120.dp)
-                .width(120.dp)
-        )
-        Image(
-            painter = painterResource(R.drawable.user02_normal),
-            contentDescription = "winner2",
-            modifier = Modifier
-                .constrainAs(winner2) {
-                    top.linkTo(textWinner.bottom)
-                    bottom.linkTo(textLoser.top)
-                    start.linkTo(winner1.end)
-                }
-                .padding(start = 12.dp)
-                .height(120.dp)
-                .width(120.dp)
-        )
-        Text(
-            text = "LOSER",
-            fontSize = 32.sp,
-            modifier = Modifier
-                .constrainAs(textLoser) {
-                    top.linkTo(dialog.top)
-                    bottom.linkTo(dialog.bottom)
-                    start.linkTo(dialog.start)
-                }
-                .padding(start = 32.dp)
-        )
-        Image(
-            painter = painterResource(R.drawable.user03_normal),
-            contentDescription = "loser1",
-            modifier = Modifier
-                .constrainAs(loser1) {
-                    top.linkTo(textLoser.bottom)
-                    start.linkTo(textWinner.start)
-                }
-                .padding(top = 24.dp)
-                .height(120.dp)
-                .width(120.dp)
-        )
-        Image(
-            painter = painterResource(R.drawable.user02_normal),
-            contentDescription = "loser2",
-            modifier = Modifier
-                .constrainAs(loser2) {
-                    top.linkTo(textLoser.bottom)
-                    start.linkTo(loser1.end)
-                }
-                .padding(top = 24.dp, start = 12.dp)
-                .height(120.dp)
-                .width(120.dp)
-        )
-        Image(
-            painter = painterResource(R.drawable.button_close),
-            contentDescription = "button_close",
-            modifier = Modifier
-                .constrainAs(btClose) {
-                    top.linkTo(loser1.bottom)
-                    end.linkTo(parent.end)
-                    start.linkTo(parent.start)
-                }
-                .padding(top = 52.dp)
-                .height(80.dp)
-                .width(160.dp)
-        )
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "RESULT",
+                fontSize = 32.sp,
+                modifier = Modifier
+                    .padding(bottom = 12.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = "WINNER",
+                fontSize = 32.sp,
+                modifier = Modifier
+                    .padding(start = 32.dp)
+            )
+            Row {
+                Image(
+                    painter = painterResource(R.drawable.user01_normal),
+                    contentDescription = "winner1",
+                    modifier = Modifier
+                        .height(120.dp)
+                        .width(120.dp)
+                )
+                Image(
+                    painter = painterResource(R.drawable.user02_normal),
+                    contentDescription = "winner2",
+                    modifier = Modifier
+                        .padding(start = 12.dp)
+                        .height(120.dp)
+                        .width(120.dp)
+                )
+            }
+            Text(
+                text = "LOSER",
+                fontSize = 32.sp,
+                modifier = Modifier
+                    .padding(start = 32.dp)
+            )
+            Row {
+                Image(
+                    painter = painterResource(R.drawable.user03_normal),
+                    contentDescription = "loser1",
+                    modifier = Modifier
+                        .padding(top = 24.dp)
+                        .height(120.dp)
+                        .width(120.dp)
+                )
+                Image(
+                    painter = painterResource(R.drawable.user02_normal),
+                    contentDescription = "loser2",
+                    modifier = Modifier
+                        .padding(top = 24.dp, start = 12.dp)
+                        .height(120.dp)
+                        .width(120.dp)
+                )
+            }
+            Image(
+                painter = painterResource(R.drawable.button_close),
+                contentDescription = "button_close",
+                modifier = Modifier
+                    .padding(top = 52.dp)
+                    .height(80.dp)
+                    .width(160.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
     }
 }
 
