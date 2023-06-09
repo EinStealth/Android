f23ed5591dc846bccbc3e070ca01afb7fc4dd9c9
atomic <daimu0307misutiru@gmail.com>
2023-06-09 13:21:23 +0900

    鬼を表示する画面を作成

--- app/src/main/java/com/example/hideandseek/ui/view/HideTimeScreen.kt
+++ app/src/main/java/com/example/hideandseek/ui/view/HideTimeScreen.kt
@@ -2,7 +2,9 @@ package com.example.hideandseek.ui.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -11,11 +13,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.hideandseek.R
@@ -59,15 +63,25 @@ private fun HideTimeLayout(
             .fillMaxSize()
             .clickable { navController.navigate("main") }
     ) {
-        Column {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "鬼は $demonName です",
+                fontSize = 24.sp,
+                color = Color.White,
+            )
+            Spacer(modifier = Modifier.weight(1f))
             Image(
                 painter = painterResource(id = selectDrawable(demonIcon)),
                 contentDescription = null,
                 modifier = Modifier
-                    .padding(start = 28.dp)
-                    .size(width = 72.dp, height = 72.dp)
+                    .size(width = 144.dp, height = 144.dp)
             )
-            Text(text = demonName)
+            Spacer(modifier = Modifier.weight(2f))
         }
     }
 }
@@ -76,7 +90,7 @@ private fun HideTimeLayout(
 @Composable
 private fun HideTimePreview() {
     HideTimeLayout(
-        demonName = "",
+        demonName = "name",
         demonIcon = 1,
         navController = rememberNavController(),
     )
